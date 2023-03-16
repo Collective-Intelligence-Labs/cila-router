1a4fa9ee9d8dbc6c7be32544a766ea8394567de7
Alexey Kulik <kulik.aliaksei@gmail.com>
2023-03-16 16:57:44 +0100

    Delete default service

--- cila.Omnichain/Services/OmnichainService.cs
+++ cila.Omnichain/Services/OmnichainService.cs
@@ -1,24 +1,6 @@
 ﻿using Grpc.Core;
-using cila.Omnichain;
 
 namespace cila.Omnichain.Services;
-
-public class GreeterService : Greeter.GreeterBase
-{
-    private readonly ILogger<GreeterService> _logger;
-    public GreeterService(ILogger<GreeterService> logger)
-    {
-        _logger = logger;
-    }
-
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new HelloReply
-        {
-            Message = "Hello " + request.Name
-        });
-    }
-}
 
 public class OmnichainService : Omnichain.OmnichainBase
 {
