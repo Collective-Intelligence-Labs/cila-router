c991e5dc2174f786694dbc0a4c7efb6eb55c3036
Alexey Kulik <kulik.aliaksei@gmail.com>
2023-03-16 17:37:17 +0100

    Random router [initial]

---
+++ cila.Omnichain/Routers/RandomRouter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace cila.Omnichain.Routers
+{
+	public enum Chains
+	{
+		Ethereum = 0,
+		Gnosis = 1
+	}
+
+	public class RandomRouter
+	{
+		public RandomRouter()
+		{
+		}
+
+		public Task<int> Route()
+		{
+			var random = new Random();
+			var route = random.Next() % 2 == 0 ? Chains.Ethereum : Chains.Gnosis;
+			return Task.FromResult((int) route);
+		}
+	}
+}
+
